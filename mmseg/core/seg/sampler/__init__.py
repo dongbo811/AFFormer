b36f46e3111501954cc8efacf6bba4d09888adae
dongbo811 <81909212+dongbo811@users.noreply.github.com>
2023-01-12 00:17:28 +0800

    Delete mmseg directory

--- mmseg/core/seg/sampler/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-Copyright (C) 2010-2022 Alibaba Group Holding Limited.
-This file is modified from:
-https://github.com/open-mmlab/mmsegmentation/blob/master/mmseg/core/seg/sampler/__init__.py
-'''
-from .base_pixel_sampler import BasePixelSampler
-from .ohem_pixel_sampler import OHEMPixelSampler
-
-__all__ = ['BasePixelSampler', 'OHEMPixelSampler']
--- mmseg/core/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-'''
-Copyright (C) 2010-2022 Alibaba Group Holding Limited.
-This file is modified from:
-https://github.com/open-mmlab/mmsegmentation/blob/master/mmseg/core/utils/__init__.py
-'''
-from .misc import add_prefix
-
-__all__ = ['add_prefix']
