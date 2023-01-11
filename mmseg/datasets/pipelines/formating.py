27f553df3a4aa78e70c8f436aa08d558f4305d08
dongbo811 <81909212+dongbo811@users.noreply.github.com>
2023-01-12 00:20:56 +0800

    Add files via upload

---
+++ mmseg/datasets/pipelines/formating.py
@@ -0,0 +1,12 @@
+'''
+Copyright (C) 2010-2022 Alibaba Group Holding Limited.
+This file is modified from:
+https://github.com/open-mmlab/mmsegmentation/blob/master/mmseg/datasets/pipelines/formating.py
+'''
+import warnings
+
+from .formatting import *
+
+warnings.warn('DeprecationWarning: mmseg.datasets.pipelines.formating will be '
+              'deprecated in 2021, please replace it with '
+              'mmseg.datasets.pipelines.formatting.')
