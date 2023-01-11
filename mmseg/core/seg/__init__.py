a0a1756b40b16644e9262bd2ca9b53eb54993c2d
dongbo811 <81909212+dongbo811@users.noreply.github.com>
2023-01-11 22:15:02 +0800

    Add files via upload

---
+++ mmseg/core/seg/__init__.py
@@ -0,0 +1,9 @@
+'''
+Copyright (C) 2010-2022 Alibaba Group Holding Limited.
+This file is modified from:
+https://github.com/open-mmlab/mmsegmentation/blob/master/mmseg/core/seg/__init__.py
+'''
+from .builder import build_pixel_sampler
+from .sampler import BasePixelSampler, OHEMPixelSampler
+
+__all__ = ['build_pixel_sampler', 'BasePixelSampler', 'OHEMPixelSampler']
---
+++ mmseg/datasets/__init__.py
@@ -0,0 +1,23 @@
+'''
+Copyright (C) 2010-2022 Alibaba Group Holding Limited.
+This file is modified from:
+https://github.com/open-mmlab/mmsegmentation/blob/master/mmseg/datasets/__inti__.py
+'''
+from .ade import ADE20KDataset
+from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
+from .cityscapes import CityscapesDataset
+from .coco_stuff import COCOStuffDataset
+from .custom import CustomDataset
+from .dataset_wrappers import (ConcatDataset, MultiImageMixDataset,
+                               RepeatDataset)
+
+
+__all__ = [
+    'CustomDataset', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
+    'DATASETS', 'build_dataset', 'PIPELINES', 'CityscapesDataset',
+    'PascalVOCDataset', 'ADE20KDataset', 'PascalContextDataset',
+    'PascalContextDataset59', 'ChaseDB1Dataset', 'DRIVEDataset', 'HRFDataset',
+    'STAREDataset', 'DarkZurichDataset', 'NightDrivingDataset',
+    'COCOStuffDataset', 'LoveDADataset', 'MultiImageMixDataset',
+    'iSAIDDataset', 'ISPRSDataset', 'PotsdamDataset'
+]
